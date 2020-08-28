c4e46b58da7de7bf28c24cc1edbff8736cce18ed
= <=>
2020-08-28 23:23:21 +0700

    morgan

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Worlddddd!')
 })
 
 app.listen(port, () => {
